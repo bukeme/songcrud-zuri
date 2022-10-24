57267c448597618dbe7b0b0b9c3676ce54feb3ab
bukeme <basseyukemme@gmail.com>
2022-10-24 12:19:47 +0100

    add models to models.py file

--- musicapp/models.py
+++ musicapp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.conf import settings
+
+
+User = settings.AUTH_USER_MODEL
 
 # Create your models here.
+
+class Artiste(models.Model):
+	first_name = models.CharField(max_length=200)
+	last_name = models.CharField(max_length=200)
+	age = models.IntegerField()
+
+
+class Song(models.Model):
+	title = models.CharField(max_length=200)
+	date_released = models.DateTimeField()
+	likes = models.ManyToManyField(User, blank=True)
+	artiste_id = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+
+class Lyrics(models.Model):
+	content = models.TextField()
+	song_id = models.ForeignKey(Song, on_delete=models.CASCADE)
